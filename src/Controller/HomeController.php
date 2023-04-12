4b511ac1aadbb0594d2c3bddc78eb67c5b3508ec
ultymaut <ultymaut@gmail.com>
2023-04-12 09:08:28 +0200

    stats in home/dahboard

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,6 +6,7 @@ use App\Entity\Absence;
 use App\Entity\Lesson;
 use App\Entity\Student;
 use App\Entity\User;
+use App\Repository\AbsenceRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,12 +16,15 @@ class HomeController extends AbstractController
 {
     
     #[Route('/', name: 'app_home')]
-    public function index(EntityManagerInterface $entityManager): Response
+    public function index(EntityManagerInterface $entityManager, AbsenceRepository $absenceRepository): Response
     {
         $users = $entityManager->getRepository(User::class)->findAll();
         $student = $entityManager->getRepository(Student::class)->findAll();
         $lesson = $entityManager->getRepository(Lesson::class)->findAll();
         $absence = $entityManager->getRepository(Absence::class)->findAll();
+        $countJustify0 = $absenceRepository ->findByExampleField0();
+        $countJustify1 = $absenceRepository ->findByExampleField1();
+        $countJustify2 = $absenceRepository ->findByExampleField2();
 
         return $this->render('dashboard.html.twig', [
             'users' => $users,
@@ -28,6 +32,9 @@ class HomeController extends AbstractController
             'lesson' => $lesson,
             'absence' => $absence,
             'controller_name' => 'HomeController',
+            'count_justify0' => $countJustify0,
+            'count_justify1' => $countJustify1,
+            'count_justify2' => $countJustify2,
         ]);
     }
     
