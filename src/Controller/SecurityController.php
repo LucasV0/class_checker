8437db1c5f1dc364c8a778948dae8ee84c2fd759
Alexandre Messuve <46402439+AlexandreMessuve@users.noreply.github.com>
2023-05-04 09:32:17 +0200

    Fix

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -7,7 +7,6 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 #use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-use Scheb\TwoFactorBundle\Security\TwoFactor\Provider\Google\GoogleAuthenticatorInterface;
 
 class SecurityController extends AbstractController
 {
