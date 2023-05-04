a4be001d4f8a58b08dae07e7894c86498f495524
Vankesbeulque Lucas <lucas.vankes@hotmail.fr>
2023-05-04 09:58:22 +0200

    stats maj

--- public/assets/js/demo/barStatsLesson.js
+++ public/assets/js/demo/barStatsLesson.js
@@ -1,48 +1,48 @@
-//@author baptiste Caron
-// Graphique d'histogramme permettant d'afficher les données d'un cours
-
-function bar (present, justifiee, non_justifiee){
-    Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
-    Chart.defaults.global.defaultFontColor = '#858796';
-    var ctx = document.getElementById("bar");
-    var myBar = new Chart(ctx,{
-        type: 'bar',
-        data: {
-            labels: ["Présent", "Justifiée", "Non-Justifiée"],
-            datasets: [{
-                axis : 'y',
-                data: [present, justifiee, non_justifiee],
-                fill : false,
-                backgroundColor: ['#4e73df', '#1cc88a', '#e74a3b'],
-                hoverBackgroundColor: ['#2e59d9', '#17a673', '#b91111'],
-                hoverBorderColor: "rgba(234, 236, 244, 1)",
-            }],
-        },
-        options: {
-            scales:{
-                xAxes :[
-                    {
-                        ticks : {
-                            fontSize : 30
-                        }
-                    }
-                ],
-                yAxes : [
-                    {
-                        ticks : {
-                            fontSize : 20,
-                            stepSize : 1,
-                            min : 0,
-                            max: 15,
-                        }
-                    }
-                ]
+function bar(present, justifiee, non_justifiee, id) {
+    Chart.defaults.global.defaultFontFamily =
+      "Nunito', '-apple-system,system-ui,BlinkMacSystemFont,'Segoe UI',Roboto,'Helvetica Neue',Arial,sans-serif";
+    Chart.defaults.global.defaultFontColor = "#858796";
+    var ctx = document.getElementById(id);
+    var myBar = new Chart(ctx, {
+      type: "bar",
+      data: {
+        labels: ["Présent", "Justifiée", "Non-justifiée"],
+        datasets: [
+          {
+            axis: "y",
+            data: [present, justifiee, non_justifiee],
+            fill: false,
+            backgroundColor: ["#4e73df", "#1cc88a", "#e74a3b"],
+            hoverBackgroundColor: ["#2e59d9", "#17a673", "#b91111"],
+            hoverBorderColor: "rgba(234, 236, 244, 1)",
+          },
+        ],
+      },
+      options: {
+        scales: {
+          xAxes: [
+            {
+              ticks: {
+                fontSize: 30,
+              },
             },
-            legend: {
-                display: false
+          ],
+          yAxes: [
+            {
+              ticks: {
+                fontSize: 20,
+                stepSize: 1,
+                min: 0,
+                max: 15,
+              },
             },
-            cutoutPercentage: 80,
+          ],
         },
-
-    })
-}
+        legend: {
+          display: false,
+        },
+        cutoutPercentage: 80,
+      },
+    });
+  }
+  
