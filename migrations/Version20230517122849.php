01153e492e815d9f31483b3125c7393776b8d004
maksen <maksen.ichapp@gmail.com>
2023-05-17 14:50:08 +0200

    eeeee

--- migrations/Version20230517122849.php
+++ migrations/Version20230517122849.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230516125608 extends AbstractMigration
+final class Version20230517122849 extends AbstractMigration
 {
     public function getDescription(): string
     {
